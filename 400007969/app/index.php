f5d4fc9e351b588aae94ad31201c778ea1a0a5ee
Aaron Harris <aaronah02@icloud.com>
2023-10-08 13:55:08 -0400

    Add tailwindcss for CSS Framework of choice # As we were allowed to use a CSS Framework

---
+++ 400007969/app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Register</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./400007969/**/*.{html,js,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
