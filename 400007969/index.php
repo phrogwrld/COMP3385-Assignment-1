499b0003d5a1853ddf57d0fc44d6bf502fdafa57
Aaron Harris <aaronah02@icloud.com>
2023-10-11 21:54:08 -0400

    Created a Router to handle routes & controllers

--- 400007969/index.php
+++ 400007969/index.php
@@ -1,7 +1,31 @@
 <?php
 use App\Controllers\DashboardController;
+use App\Controllers\LoginController;
+use App\Controllers\RegisterController;
 
 require './app/autoload.php';
 
-$dashboardController = new DashboardController();
-$dashboardController->render();
+// $dashboardController = new DashboardController();
+// $dashboardController->render();
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$uriParts = explode('/', trim($requestUri, '/'));
+
+$route = isset($uriParts[1]) ? $uriParts[1] : '';
+
+switch ($route) {
+	case 'dashboard':
+		$dashboardController = new DashboardController();
+		$dashboardController->render();
+		break;
+	case 'login':
+		$loginController = new LoginController();
+		$loginController->render();
+		break;
+	case 'register':
+		$registerController = new RegisterController();
+		$registerController->render();
+		break;
+	default:
+		echo '404';
+}
